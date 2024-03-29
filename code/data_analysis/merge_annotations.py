ef3ec6659699a6ff97b07d476689f7f920464e95
Kristin Pun <kristinpun@Kristins-MacBook-Air.local>
2024-03-29 22:37:56 +0100

    update

--- code/data_analysis/merge_annotations.py
+++ code/data_analysis/merge_annotations.py
@@ -28,13 +28,20 @@ for file in FILES:
             if re.match(r"\d+,[0-4],[0-4],[0-4],[0-4]", ",".join(entry)):
                 merged_annotation.append(entry)
 
-annotation = sorted(merged_annotation, key=lambda x: int(x[0]))    
+annotation = sorted(merged_annotation, key=lambda x: int(x[0])) 
+
+# for the extra two columns for model answer
+new_annotation = []
+for inner in annotation:
+    if len(inner) == 6:
+        new_annotation.append(inner[:-1])
+    else:
+        new_annotation.append(inner[:-2])
 
 # Retrieve type task and add as column
 data = json.load(open("merged_data_1500.json"))
 
-
-for datapoint in annotation:
+for datapoint in new_annotation:
     id = int(datapoint[0])
     for entry in data:
         if entry["id"] == id:
@@ -45,6 +52,6 @@ for datapoint in annotation:
     
 #     csv.writer(out_file, lineterminator="\n").writerows([["id","structure","informativity","on-topic","correctness","category"]]+annotation)
 
-with open("merged_llama_annotation_"+str(len(annotation))+".csv", "w", encoding="utf-8") as out_file:
+with open("merged_llama_annotation_"+str(len(new_annotation))+".csv", "w", encoding="utf-8") as out_file:
     
-    csv.writer(out_file, lineterminator="\n").writerows([["id","structure","informativity","on-topic","correctness","category"]]+annotation)
+    csv.writer(out_file, lineterminator="\n").writerows([["id","structure","informativity","on-topic","correctness","category"]]+new_annotation)
